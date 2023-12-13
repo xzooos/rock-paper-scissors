d1d979ed4ae288d75669d1ef747b622e2fb8c630
Zorana <zoranajankelic97@gmail.com>
2023-12-13 17:47:43 +0100

    Write playRound function simulating a round of rock paper scissors played between a computer and a user

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,3 +4,18 @@ function getComputerChoice() {
     (randomNumber === 1) ? 'paper':
     'scissors';
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if ((playerSelection === 'rock' && computerSelection === 'scissors') || 
+    (playerSelection === 'paper' && computerSelection === 'rock') || 
+    (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'scissors') ||
+    (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    } else {
+        return `It's a tie! Both users chose ${playerSelection}!`;
+    }
+}
