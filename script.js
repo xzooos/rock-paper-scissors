3b9c5296ff729295d4d9e4f5a973ee60ac8133a1
Zorana <zoranajankelic97@gmail.com>
2023-12-20 19:11:53 +0100

    Rewrite getComputerChoice function using arrays

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3);
-    return (randomNumber === 0) ? 'rock':
-    (randomNumber === 1) ? 'paper':
-    'scissors';
+    const randomChoice = ['rock', 'paper', 'scissors'];
+    return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
